e03367867dc084679d206cd0e9671483382ff4d0
Matt Elphick <matt.a.elphy@gmail.com>
2023-09-02 22:23:20 +0100

    feat: Handle empty/wrong scope characters better

--- src/pages/api/gw2-build/[key]/[character].ts
+++ src/pages/api/gw2-build/[key]/[character].ts
@@ -35,6 +35,17 @@ const handler: NextApiHandler = async (req, response) => {
   }
 }
 
+function prepareFetchItemsById(apiKey: string, abortController: AbortController) {
+  return async (type: string, ids: any[]) => {
+    if (ids.length === 0) return new Map()
+    return fetch(`https://api.guildwars2.com/v2/${type}?access_token=${apiKey}&ids=${ids}`, {
+      signal: abortController.signal,
+    })
+      .then((r) => r.json())
+      .then((d) => new Map(d.map((i) => [i.id, i])))
+  }
+}
+
 async function getNormal(
   abortController: AbortController,
   originalApiKey: string,
@@ -79,8 +90,12 @@ async function getNormal(
       delete characterData.backstory
       delete characterData.crafting
 
+      if (!characterData.equipment) {
+        logger.warn("Missing equipment, likely token didn't allow builds permission")
+      }
+
       logger.do('skins')
-      const skins = characterData.equipment.map((item) => item?.skin).filter(Boolean)
+      const skins = (characterData.equipment || []).map((item) => item?.skin).filter(Boolean)
       logger.do('skills')
       const skills = Object.values(characterData.skills || {})
         .flatMap((s: any) => [s.heal, s.elite, s.utilities])
@@ -97,49 +112,20 @@ async function getNormal(
       const equipmentIds = [characterData?.equipment_pvp?.rune]
         .filter(Boolean)
         .concat(
-          characterData.equipment
+          (characterData.equipment || [])
             .flatMap((item) => [item?.id, item?.upgrades, item?.infusions].filter(Boolean))
             .filter(Boolean)
         )
 
       console.info('[start:extra]')
 
+      const itemFetcher = prepareFetchItemsById(apiKey, abortController)
       const [equipmentData, skinData, skillData, traitData, specializationData, amuletData] = await Promise.all([
-        fetch(`https://api.guildwars2.com/v2/items?access_token=${apiKey}&ids=${equipmentIds}`, {
-          signal: abortController.signal,
-        })
-          .then((r) => r.json())
-          .then((d) => new Map(d.map((i) => [i.id, i]))),
-        skins.length === 0
-          ? new Map()
-          : fetch(`https://api.guildwars2.com/v2/skins?access_token=${apiKey}&ids=${skins}`, {
-              signal: abortController.signal,
-            })
-              .then((r) => r.json())
-              .then((d) => new Map(d.map((i) => [i.id, i]))),
-        skills.length === 0
-          ? new Map()
-          : fetch(`https://api.guildwars2.com/v2/skills?access_token=${apiKey}&ids=${skills}`, {
-              signal: abortController.signal,
-            })
-              .then((r) => r.json())
-              .then((d) => {
-                return new Map(d.map((i) => [i.id, i]))
-              }),
-        traits.length === 0
-          ? new Map()
-          : fetch(`https://api.guildwars2.com/v2/traits?access_token=${apiKey}&ids=${traits}`, {
-              signal: abortController.signal,
-            })
-              .then((r) => r.json())
-              .then((d) => new Map(d.map((i) => [i.id, i]))),
-        specializations.length === 0
-          ? new Map()
-          : fetch(`https://api.guildwars2.com/v2/specializations?access_token=${apiKey}&ids=${specializations}`, {
-              signal: abortController.signal,
-            })
-              .then((r) => r.json())
-              .then((d) => new Map(d.map((i) => [i.id, i]))),
+        itemFetcher('items', equipmentIds),
+        itemFetcher('skins', skins),
+        itemFetcher('skills', skills),
+        itemFetcher('traits', traits),
+        itemFetcher('specializations', specializations),
         !characterData?.equipment_pvp?.amulet
           ? undefined
           : fetch(
